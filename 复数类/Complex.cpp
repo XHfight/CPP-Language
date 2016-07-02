309324fda7f508a7902c431355228d999e7cbd68
XHfight <X_H_fight@163.com>
2016-07-02 14:35:26 +0800

    Complex -2

--- 复数类/Complex.cpp
+++ 复数类/Complex.cpp
@@ -1,62 +1,97 @@
 #include <iostream>
 using namespace std;
 
-class Complax
+class Complex
 {
 public:
-	Complax(double real = 0.0,double image = 0.0)
+	Complex(double real = 0.0, double image = 0.0)
 	{
 		_real = real;
 		_image = image;
 	}
 
-	Complax(const Complax& c)
+	~Complex()
 	{
-		_real = c._real;
-		_image = c._image;
+	
+	}
+	void Display()
+	{
+		cout << _real << "+(" << _image << ")i" <<endl;
 	}
 
-	Complax& operator=(const Complax& c)
+	Complex operator+(const Complex& c)
 	{
-		if(*this == c)
-		{
-			return *this;
-		}
+		Complex ret;
+		ret._real = _real + c._real;
+		ret._image = _image + c._image;
+		return ret;
+	}
 
-		_real = c._real;
-		_image = c._image;
+	Complex operator-(const Complex& c)
+	{
+		return Complex(_real-c._real, _image-c._image);
+	}
 
+	Complex operator*(const Complex& c)
+	{
+		Complex ret;
+		ret._real = _real*c._real-_image*c._image;
+		ret._image = _image*c._real + _real*c._image;
+		return ret;
+	}
+
+	Complex operator/(const Complex& c)
+	{
+		Complex ret;
+		ret._real = (_real*c._real+_image*c._image)/(c._real*c._real+c._image*c._image);
+		ret._image = (_image*c._real-_real*c._image)/(c._real*c._real+c._image*c._image);
+		return ret;
+	}
+
+	Complex operator+=(const Complex& c)
+	{
+
+		_real += c._real;
+		_image += c._image;
 		return *this;
+	}
 
-	}
-	bool operator==(const Complax& c)
+	Complex operator-=(const Complex& c)
 	{
-		return (_real == c._real)&&(_image == c._image);		
+		_real -= c._real;
+		_image -= c._image;
+		return *this;
 	}
-	void Print()
+
+	Complex operator*=(const Complex& c)
 	{
-		cout << "real= "<<_real<<"image="<<_image<<endl;
+		_real = _real*c._real-_image*c._image;
+		_image = _image*c._real + _real*c._image;
+		return *this;
 	}
-private:
-	bool IsEqual(const Complax& c)
+
+	Complex operator/=(const Complex& c)
 	{
-		return (_real == c._real)&&(_image == c._image);
+		_real = (_real*c._real+_image*c._image)/(c._real*c._real+c._image*c._image);
+		_image = (_image*c._real-_real*c._image)/(c._real*c._real+c._image*c._image);
+		return *this;
 	}
+
 private:
 	double _real;
 	double _image;
 };
+
 int main()
 {
-	Complax c1(1.0, 2.0);
-	Complax c2(c1);
-	Complax c3;
+	Complex c1(1.0, 2.0);
+	c1.Display();
+	Complex c2(2.0, 3.0);
+	c2.Display();
+//	Complex c3 = c1+c2;
+//	Complex c3 = c1-c2;
 
-	c3 = c1;
-	c1.Print();
-	c2.Print();
-	c3.Print();
-
+//	c3.Display();
 	system("pause");
 	return 0;
 }

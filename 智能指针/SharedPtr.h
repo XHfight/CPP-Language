170b4d56869937344f2a4108dcb4757f44ee1bf2
XHfight <X_H_fight@163.com>
2016-09-04 12:06:25 +0800

    定制删除器版本

--- 智能指针/SharedPtr.h
+++ 智能指针/SharedPtr.h
@@ -2,21 +2,32 @@
 #include <iostream>
 using namespace std;
 
-template <class T>
+struct DefaultDel
+{
+	void operator()(void *ptr)
+	{
+		cout<<"del"<<endl;
+		delete ptr;
+	}
+};
+
+template <class T,class D = DefaultDel>
 class SharedPtr
 {
 public:
-	SharedPtr(T* ptr)
+	SharedPtr(T* ptr = NULL, D& d = DefaultDel())
 		:_ptr(ptr)
-		:_PCount(new int(1))
+		,_PCount(new int(1))
+		,_del(d)
 	{}
 	SharedPtr(const SharedPtr<T>& shp)
 		:_ptr(shp._ptr)
 		,_PCount(shp._PCount)
+		,_del(shp._del)
 	{
 		++GetCount();
 	}
-	SharedPtr& operator=(const SharedPtr<T>& shp)
+	/*SharedPtr& operator=(const SharedPtr<T>& shp)
 	{
 		if(_ptr != shp._ptr)
 		{
@@ -26,7 +37,16 @@ public:
 			++GetCount();
 		}
 		return *this;
+	}*/
+
+	//现代版写法
+	SharedPtr& operator=(SharedPtr sp)
+	{
+		swap(_ptr,sp._ptr);
+		swap(_PCount,sp._PCount);
+		return *this;
 	}
+
 	~SharedPtr()
 	{
 		Release();
@@ -50,7 +70,8 @@ private:
 	{
 		if(--GetCount() == 0)
 		{
-			delete _ptr;
+			_del(_ptr); //仿函数
+
 			_ptr = NULL;
 			delete _PCount;
 			_PCount = NULL;
@@ -63,4 +84,5 @@ private:
 private:
 	T* _ptr;
 	int* _PCount;
+	D _del;
 };
